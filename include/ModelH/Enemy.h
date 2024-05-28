a16401ba85cef9057a0e312fa706947c00f6e850
Sluchai40 <mabbolos8@gmail.com>
2024-05-28 02:41:50 +0200

    [CREATE] new Enemy classe

---
+++ include/ModelH/Enemy.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include "./ModelH/Observable.h"
+
+class Enemy : public Observable {
+
+public:
+    Enemy(int life, int speed, int startX, int startY);
+
+    int getLifePoints() const;
+    void setLifePoints(int lp);
+
+    int getSpeed() const;
+    void setSpeed(int spd);
+
+    vector<int> getPosition() const;
+    void setPosition(int x, int y);
+
+    void takeDamage(int damage);
+    void move(int x, int y);
+    
+private:
+    int lifePoints;
+    int speed;
+    vector<int> position;
+};
