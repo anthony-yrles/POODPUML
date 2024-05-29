32df8d8cb860a00faf93f21c4cfdc047e0955a7e
Sluchai40 <mabbolos8@gmail.com>
2024-05-29 04:02:52 +0200

    [MODIFY] new method that shoud normally draw a map with rect of differente colo, not working for the moment

--- include/VueH/GuiInGame.h
+++ include/VueH/GuiInGame.h
@@ -1,18 +1,22 @@
 #pragma once
 
 #include <.\VueH\Draw.h>
+#include <./ControllerH/MapController.h>
 
 class GuiInGame {
     public:
         GuiInGame(int WIDTH, int HEIGHT, int mouseX, int mouseY, bool clicked, bool running, SDL_Event evenement);
     
         bool drawInGame(int WIDTH, int HEIGHT, int mouseX, int mouseY, bool clicked, bool running, SDL_Event evenement);
+        void drawMap(const string& filename, SDL_Renderer* gameRenderer, int x, int y, Draw* draw);
 
     private:
         int WIDTH, HEIGHT;
         int mouseX, mouseY;
         bool clicked;
         bool running;
+        vector<vector<Tile>> tiles;
+
         SDL_Event evenement;
         SDL_Window* gameWindow;
         SDL_Renderer* gameRenderer;
--- src/VueCpp/GuiInGame.cpp
+++ src/VueCpp/GuiInGame.cpp
@@ -1,6 +1,6 @@
 #include "./VueH/GuiInGame.h"
 
-GuiInGame::GuiInGame(int WIDTH, int HEIGHT, int mouseX, int mouseY, bool clicked, bool running, SDL_Event evenement){
+GuiInGame::GuiInGame(int WIDTH, int HEIGHT, int mouseX, int mouseY, bool clicked, bool running, SDL_Event /*evenement*/) {
     this->WIDTH = WIDTH;
     this->HEIGHT = HEIGHT;
     this->mouseX = mouseX;
@@ -15,6 +15,7 @@ bool GuiInGame::drawInGame(int WIDTH, int HEIGHT, int mouseX, int mouseY, bool c
     Draw draw(clicked);
     gameWindow = draw.createWindow(WIDTH, HEIGHT, "Game", gameWindow);
     gameRenderer = draw.createRenderer(gameWindow, gameRenderer);
+    drawMap("./assets/map/map.txt", gameRenderer, WIDTH -300, HEIGHT, &draw);
 
     // Game loop
     while (!running) {
@@ -36,4 +37,30 @@ bool GuiInGame::drawInGame(int WIDTH, int HEIGHT, int mouseX, int mouseY, bool c
 
     }
     return running;
+}
+
+void GuiInGame::drawMap(const string& filename, SDL_Renderer* gameRenderer, int width, int height, Draw* draw) {
+    // Créez le contrôleur de la carte et obtenez toutes les tuiles
+    MapController mapController;
+    Map map(width, height);
+    std::vector<std::vector<Tile>> tiles = mapController.createAndReturnMap(filename, width, height, &map);
+
+    // Parcourez les tuiles et dessinez-les
+    for (long long unsigned int i = 0; i < tiles.size(); ++i) {
+        for (long long unsigned int j = 0; j < tiles[i].size(); ++j) {
+            if (tiles[i][j].isEmpty) {
+                draw->drawRect(gameRenderer, i * 32, j * 32, 32, 32, 0, 255, 0, 255);
+            } else if (tiles[i][j].isMonsterPath) {
+                draw->drawRect(gameRenderer, i * 32, j * 32, 32, 32, 0, 0, 255, 255);
+            } else if (tiles[i][j].isMonsterBegin) {
+                draw->drawRect(gameRenderer, i * 32, j * 32, 32, 32, 255, 0, 0, 255);
+            } else if (tiles[i][j].isMonsterEnd) {
+                draw->drawRect(gameRenderer, i * 32, j * 32, 32, 32, 255, 0, 0, 255);
+            } else if (tiles[i][j].isTowerEmplacement) {
+                draw->drawRect(gameRenderer, i * 32, j * 32, 32, 32, 0, 0, 255, 255);
+            } else if (tiles[i][j].isDecoration) {
+                draw->drawRect(gameRenderer, i * 32, j * 32, 32, 32, 255, 255, 0, 255);
+            }
+        }
+    }
 }
