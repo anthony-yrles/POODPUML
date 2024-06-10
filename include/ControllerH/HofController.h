8fbbbe559e37d21315fae1c90f4ddec63c2fd5ee
Sluchai40 <mabbolos8@gmail.com>
2024-06-10 04:33:29 +0200

    [DOCUMENTATION] documentation for HofController classe

--- include/ControllerH/HofController.h
+++ include/ControllerH/HofController.h
@@ -1,21 +1,36 @@
 #pragma once
-#include "./ModelH/Observable.h"
-#include "./ModelH/Observer.h"
 
+#include "./ModelH/Observable.h" // Include necessary header files
+#include "./ModelH/Observer.h"
 #include "./ModelH/HallOfFame.h"
 
-class HofController : public Observable, public Observer{
+// HofController class declaration, inheriting from Observable and Observer
+class HofController : public Observable, public Observer {
 public:
+    // Constructor
     HofController();
+    
+    // Destructor
     ~HofController();
 
+    // Method to read data from a text file
     void readTxtFile();
+    
+    // Method to write data to a text file
     void writeTxtFile(const string& newName, const string& newDifficulty, const string& newLevel);
+    
+    // Method to sort data by difficulty level
     void sortDataByDifficulty(int difficultyReceived);
+    
+    // Method to get the top three sorted data
     void topThreeSortedData(int difficultyReceived);
+    
+    // Method to get sorted data from HallOfFame
     vector<Data> getSortedDataFoh() const;
+    
+    // Observer update method
     void update() override;
 
 private:
-    HallOfFame hallOfFame;
+    HallOfFame hallOfFame; // Instance of HallOfFame
 };
--- src/ControllerCpp/HofController.cpp
+++ src/ControllerCpp/HofController.cpp
@@ -1,28 +1,41 @@
 #include "./ControllerH/HofController.h"
 
+// Constructor
 HofController::HofController() {
-    hallOfFame.addObserver(this);
+    hallOfFame.addObserver(this); // Add this instance as an observer to the HallOfFame
 }
 
+// Destructor
 HofController::~HofController() {
-    hallOfFame.removeObserver(this);
+    hallOfFame.removeObserver(this); // Remove this instance as an observer from the HallOfFame
 }
 
+// Method to read data from a text file
 void HofController::readTxtFile() {
     hallOfFame.readTxtFile();
 }
+
+// Method to write data to a text file
 void HofController::writeTxtFile(const string& newName, const string& newDifficulty, const string& newLevel) {
     hallOfFame.writeTxtFile(newName, newDifficulty, newLevel);
 }
+
+// Method to sort data by difficulty level
 void HofController::sortDataByDifficulty(int difficultyReceived) {
     hallOfFame.sortDataByDifficulty(difficultyReceived);
 }
+
+// Method to get the top three sorted data
 void HofController::topThreeSortedData(int difficultyReceived) {
     hallOfFame.topThreeSortedData(difficultyReceived);
 }
+
+// Method to get sorted data from HallOfFame
 vector<Data> HofController::getSortedDataFoh() const {
     return hallOfFame.getSortedData();
 }
+
+// Observer update method
 void HofController::update() {
-    notifyObservers();
+    notifyObservers(); // Notify observers when there's an update
 }
