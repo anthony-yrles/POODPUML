0bbf19bf26f280c285757228a8bb37aef1b0dd30
Sluchai40 <mabbolos8@gmail.com>
2024-06-10 04:40:01 +0200

    [DOCUMENTATION] documentation for OptionController classe

--- include/ControllerH/OptionController.h
+++ include/ControllerH/OptionController.h
@@ -4,17 +4,26 @@
 
 class OptionController {
 public:
+    // Constructor
     OptionController();
+    // Destructor
     ~OptionController();
     
+    // Load music file
     void loadMusic(const string& music);
+    // Play loaded music
     void playMusic();
+    // Decrease volume
     void decreaseVolume();
+    // Increase volume
     void increaseVolume();
+    // Get current volume level
     int getVolume();
+    // Get current difficulty level
     int getDifficulty();
+    // Set new difficulty level
     void setDifficulty(int newDifficulty);
 
 private:
-    Option* options;
+    Option* options; // Pointer to Option object
 };
--- src/ControllerCpp/OptionController.cpp
+++ src/ControllerCpp/OptionController.cpp
@@ -1,30 +1,45 @@
 #include "./ControllerH/OptionController.h"
 #include <iostream>
 
+// Constructor
 OptionController::OptionController() {
-    options = Option::getInstance();
+    options = Option::getInstance(); // Get instance of Option object
 }
 
+// Destructor
 OptionController::~OptionController() {}
 
+// Load music file
 void OptionController::loadMusic(const std::string& music) {
     options->loadMusic(music);
 }
+
+// Play loaded music
 void OptionController::playMusic() {
     options->playMusic();
 }
+
+// Decrease volume
 void OptionController::decreaseVolume() {
     options->decreaseVolume();
 }
+
+// Increase volume
 void OptionController::increaseVolume() {
     options->increaseVolume();
 }
+
+// Get current volume level
 int OptionController::getVolume() {
     return options->getVolume();
 }
+
+// Get current difficulty level
 int OptionController::getDifficulty() {
     return options->getDifficulty();
 }
+
+// Set new difficulty level
 void OptionController::setDifficulty(int newDifficulty) {
     options->setDifficulty(newDifficulty);
 }
